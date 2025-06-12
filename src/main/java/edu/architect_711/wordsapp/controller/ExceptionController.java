4383108e8e13b3e47e5b165b7b1762a7a8cebbe2
45p9uerhiu <jfjjjnqhqiriykkn@gmail.com>
2025-06-12 15:54:21 +0300

    feat!: add raw exception handling controller

---
+++ src/main/java/edu/architect_711/wordsapp/controller/ExceptionController.java
@@ -0,0 +1,32 @@
+package edu.architect_711.wordsapp.controller;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ExceptionController {
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<ErrorResponse> handleRuntimeException(RuntimeException e) {
+        System.out.println("RuntimeException: " + e.getMessage());
+        return new ResponseEntity<>(new ErrorResponse(
+                e.getMessage(),
+                e.getLocalizedMessage()
+        ), HttpStatus.BAD_REQUEST);
+    }
+
+    @Data @NoArgsConstructor @AllArgsConstructor
+    public static class ErrorResponse {
+        private String message;
+        private String localizedMessage;
+
+        @Override
+        public String toString() {
+            return "ErrorResponse [message=" + message + ", localizedMessage=" + localizedMessage + "]";
+        }
+    }
+}
