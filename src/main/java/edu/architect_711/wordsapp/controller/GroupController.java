818492d3dd19c9ebe710d07b5cf1bd24dfa581fa
45p9uerhiu <jfjjjnqhqiriykkn@gmail.com>
2025-06-16 23:54:14 +0300

    feat: add group controller

---
+++ src/main/java/edu/architect_711/wordsapp/controller/GroupController.java
@@ -0,0 +1,36 @@
+package edu.architect_711.wordsapp.controller;
+
+import edu.architect_711.wordsapp.model.dto.GroupDto;
+import edu.architect_711.wordsapp.model.dto.SaveGroupDto;
+import edu.architect_711.wordsapp.model.dto.UpdateGroupDto;
+import edu.architect_711.wordsapp.service.group.GroupService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController @RequestMapping("/api/groups")
+@RequiredArgsConstructor
+public class GroupController {
+    private final GroupService groupService;
+    
+    @GetMapping
+    public List<GroupDto> get() {
+        return groupService.get();
+    }
+
+    @PostMapping
+    public GroupDto save(@RequestBody SaveGroupDto groupDto) {
+        return groupService.save(groupDto);
+    }
+    
+    @PutMapping
+    public GroupDto update(@RequestBody UpdateGroupDto updateGroupDto) {
+        return groupService.update(updateGroupDto);
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable Long id) {
+        groupService.delete(id);
+    }
+}
