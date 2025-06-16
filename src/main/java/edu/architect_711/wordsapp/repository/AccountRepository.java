2e47d65ba955419241190e039bb46f2b1da51302
45p9uerhiu <jfjjjnqhqiriykkn@gmail.com>
2025-06-16 23:47:29 +0300

    feat: add safe find first method

--- src/main/java/edu/architect_711/wordsapp/repository/AccountRepository.java
+++ src/main/java/edu/architect_711/wordsapp/repository/AccountRepository.java
@@ -3,9 +3,11 @@ package edu.architect_711.wordsapp.repository;
 import edu.architect_711.wordsapp.model.entity.Account;
 import jakarta.persistence.EntityNotFoundException;
 
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
+import java.util.List;
 import java.util.Optional;
 
 
@@ -13,12 +15,16 @@ public interface AccountRepository extends JpaRepository<Account, Long> {
     Optional<Account> findByUsername(String username);
 
 
-
     default Account safeFindByUsername(String username) throws UsernameNotFoundException {
         return this.findByUsername(username).orElseThrow(() -> new UsernameNotFoundException("User not found with username: " + username));
     }
 
-    default Account safeFindById(Long id) throws EntityNotFoundException {
-        return findById(id).orElseThrow(() -> new EntityNotFoundException("Entity with id not found: " + id));
+    default Account safeFindFirst() throws EntityNotFoundException {
+        List<Account> content = findAll(PageRequest.of(0, 1)).getContent();
+
+        if (content.isEmpty())
+            throw new EntityNotFoundException("There MUST be at least one user in the databse, but found nothing");
+
+        return content.getFirst();
     }
 }
