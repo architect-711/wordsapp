80f5cb98c75f0a57348e7548fcf30b85feb94a8a
45p9uerhiu <jfjjjnqhqiriykkn@gmail.com>
2025-06-16 23:40:30 +0300

    feat: add group repo

---
+++ src/main/java/edu/architect_711/wordsapp/repository/GroupRepository.java
@@ -0,0 +1,28 @@
+package edu.architect_711.wordsapp.repository;
+
+import edu.architect_711.wordsapp.model.entity.Group;
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+public interface GroupRepository extends JpaRepository<Group, Long> {
+
+    @Query(
+        nativeQuery = true,
+        value = """
+                select * from
+                    account_group
+                where
+                    account_group.owner_id = :userId
+                """ 
+    )
+    List<Group> findAll(@Param("userId") Long userId);
+
+    default Group safeFindById(Long id) throws EntityNotFoundException {
+        return findById(id).orElseThrow(() -> new EntityNotFoundException("Group with such id not found: " + id));
+    }
+
+}
