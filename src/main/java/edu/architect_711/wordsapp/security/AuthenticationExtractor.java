53bdba5914deac31c1a077756021a16c7974e3c6
45p9uerhiu <jfjjjnqhqiriykkn@gmail.com>
2025-06-16 23:50:05 +0300

    fix: redo logic of the auth extractor

--- src/main/java/edu/architect_711/wordsapp/security/AuthenticationExtractor.java
+++ src/main/java/edu/architect_711/wordsapp/security/AuthenticationExtractor.java
@@ -2,29 +2,24 @@ package edu.architect_711.wordsapp.security;
 
 import edu.architect_711.wordsapp.model.dto.AccountDetails;
 import edu.architect_711.wordsapp.model.entity.Account;
-import edu.architect_711.wordsapp.repository.AccountRepository;
-import jakarta.persistence.EntityNotFoundException;
-import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.stereotype.Component;
 
-@Component
-@RequiredArgsConstructor
 public class AuthenticationExtractor {
-    private final AccountRepository accountRepository;
 
     /**
-     * retrieve authenticated user from the context and database consequently
-     * @return found account
-     * @throws EntityNotFoundException if the account wasn't found
+     * Retrieves {@link AccountDetails} from the {@link SecurityContextHolder}
+     * @return account details from the security context
      */
-    public Account extract() {
-        AccountDetails principal = getUserDetailsFromAuthContext();
-
-        return accountRepository.safeFindById(principal.getId());
+    public static AccountDetails getAccountDetails() {
+        return (AccountDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
     }
 
-    public AccountDetails getUserDetailsFromAuthContext() {
-        return (AccountDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+    /**
+     * Retrieves authenticated user object {@link edu.architect_711.wordsapp.model.entity.Account}
+     * from the {@link SecurityContextHolder}
+     * @return the account entity
+     */
+    public static Account getAccount() {
+        return getAccountDetails().getAccount();
     }
 }
