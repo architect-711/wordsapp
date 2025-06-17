6e21a2bfee27fa5309c016dc2a7a83864617f6fa
45p9uerhiu <jfjjjnqhqiriykkn@gmail.com>
2025-06-17 16:49:17 +0300

    refactor: clean up + imports

--- src/main/java/edu/architect_711/wordsapp/model/mapper/GroupMapper.java
+++ src/main/java/edu/architect_711/wordsapp/model/mapper/GroupMapper.java
@@ -1,21 +1,44 @@
 package edu.architect_711.wordsapp.model.mapper;
 
-import edu.architect_711.wordsapp.model.dto.GroupDto;
-import edu.architect_711.wordsapp.model.dto.SaveGroupDto;
-import edu.architect_711.wordsapp.model.dto.UpdateGroupDto;
+import edu.architect_711.wordsapp.model.dto.group.GroupDto;
+import edu.architect_711.wordsapp.model.dto.group.SaveGroupDto;
+import edu.architect_711.wordsapp.model.dto.group.UpdateGroupDto;
 import edu.architect_711.wordsapp.model.entity.Account;
 import edu.architect_711.wordsapp.model.entity.Group;
 
+import java.util.List;
+
 public class GroupMapper {
     public static Group toEntity(SaveGroupDto groupDto, Account account) {
-        return new Group(groupDto.getTitle(), null, groupDto.getDescription(), account);
-    } 
+        return new Group(
+                groupDto.getTitle(),
+                groupDto.getDescription(),
+                account
+        );
+    }
 
     public static GroupDto toDto(Group group) {
-        return new GroupDto(group.getId(), group.getTitle(), group.getCreated(), group.getDescription(), group.getAccount().getId());
+        return GroupDto.builder()
+                .id(group.getId())
+                .title(group.getTitle())
+                .created(group.getCreated())
+                .description(group.getDescription())
+                .ownerId(group.getAccount().getId())
+                .build();
     }
 
     public static UpdateGroupDto toUpdateDto(Group group) {
-        return new UpdateGroupDto(group.getId(), group.getTitle(), group.getDescription());
+        return new UpdateGroupDto(
+                group.getId(),
+                group.getTitle(),
+                group.getDescription()
+        );
+    }
+
+    public static List<GroupDto> toDto(List<Group> groups) {
+        return groups
+                .stream()
+                .map(GroupMapper::toDto)
+                .toList();
     }
 }
