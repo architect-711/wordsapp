a5864f406855db5278246de15074b326a8ffa499
45p9uerhiu <jfjjjnqhqiriykkn@gmail.com>
2025-06-17 16:50:28 +0300

    feat: add map list method

--- src/main/java/edu/architect_711/wordsapp/model/mapper/LanguageMapper.java
+++ src/main/java/edu/architect_711/wordsapp/model/mapper/LanguageMapper.java
@@ -1,7 +1,9 @@
 package edu.architect_711.wordsapp.model.mapper;
 
-import edu.architect_711.wordsapp.model.dto.LanguageDto;
+import edu.architect_711.wordsapp.model.dto.language.LanguageDto;
 import edu.architect_711.wordsapp.model.entity.Language;
+
+import java.util.List;
 
 public class LanguageMapper {
     public static Language toEntity(LanguageDto language) {
@@ -10,5 +12,12 @@ public class LanguageMapper {
 
     public static LanguageDto toDto(Language language) {
         return new LanguageDto(language.getId(), language.getTitle());
+    }
+
+    public static List<LanguageDto> toDto(List<Language> languages) {
+        return languages
+                .stream()
+                .map(LanguageMapper::toDto)
+                .toList();
     }
 }
