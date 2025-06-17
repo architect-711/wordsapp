366ca45acfc06f5295651d25d5e9f441a7e788f4
45p9uerhiu <jfjjjnqhqiriykkn@gmail.com>
2025-06-17 16:57:09 +0300

    refactor: remove unused method

--- src/main/java/edu/architect_711/wordsapp/model/mapper/LanguageMapper.java
+++ src/main/java/edu/architect_711/wordsapp/model/mapper/LanguageMapper.java
@@ -6,9 +6,6 @@ import edu.architect_711.wordsapp.model.entity.Language;
 import java.util.List;
 
 public class LanguageMapper {
-    public static Language toEntity(LanguageDto language) {
-        return new Language(language.getId(), language.getTitle());
-    }
 
     public static LanguageDto toDto(Language language) {
         return new LanguageDto(language.getId(), language.getTitle());
