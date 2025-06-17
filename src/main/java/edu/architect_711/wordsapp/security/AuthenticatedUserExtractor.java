8c823503a18289341bf2c17e10a7c36761a6984a
45p9uerhiu <jfjjjnqhqiriykkn@gmail.com>
2025-06-17 16:43:45 +0300

    refactor: rename file

--- src/main/java/edu/architect_711/wordsapp/security/AuthenticatedUserExtractor.java
+++ src/main/java/edu/architect_711/wordsapp/security/AuthenticatedUserExtractor.java
@@ -1,13 +1,17 @@
 package edu.architect_711.wordsapp.security;
 
-import edu.architect_711.wordsapp.model.dto.AccountDetails;
+import edu.architect_711.wordsapp.model.dto.account.AccountDetails;
 import edu.architect_711.wordsapp.model.entity.Account;
 import org.springframework.security.core.context.SecurityContextHolder;
 
-public class AuthenticationExtractor {
+public class AuthenticatedUserExtractor {
 
     /**
-     * Retrieves {@link AccountDetails} from the {@link SecurityContextHolder}
+     * Retrieves {@link AccountDetails} from the {@link SecurityContextHolder}.
+     * <p>
+     * Take care that the context wasn't cleared before otherwise you get the
+     * {@link NullPointerException}
+     *
      * @return account details from the security context
      */
     public static AccountDetails getAccountDetails() {
@@ -17,6 +21,10 @@ public class AuthenticationExtractor {
     /**
      * Retrieves authenticated user object {@link edu.architect_711.wordsapp.model.entity.Account}
      * from the {@link SecurityContextHolder}
+     * <p>
+     * Take care that the context wasn't cleared before otherwise you get the
+     * {@link NullPointerException}
+     *
      * @return the account entity
      */
     public static Account getAccount() {
