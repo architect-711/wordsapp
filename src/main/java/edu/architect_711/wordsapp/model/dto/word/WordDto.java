10c04f6650f46a141299bac35138db906b029c59
45p9uerhiu <jfjjjnqhqiriykkn@gmail.com>
2025-06-23 20:14:27 +0300

    feat: add word dto

---
+++ src/main/java/edu/architect_711/wordsapp/model/dto/word/WordDto.java
@@ -0,0 +1,38 @@
+package edu.architect_711.wordsapp.model.dto.word;
+
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Getter
+@Setter
+public class WordDto {
+
+    @NotNull @Min(0)
+    private Long id;
+
+    @NotBlank
+    private String title;
+
+    @NotNull @Min(0)
+    private Long languageId;
+
+    private String definition;
+
+    private String description;
+
+    private List<String> translations;
+
+    private List<String> transcriptions;
+
+    @NotNull
+    private LocalDateTime created;
+
+}
