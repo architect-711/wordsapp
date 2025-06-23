8f78aa7dd2b686c13e0d6408e3a9d5136f212eba
45p9uerhiu <jfjjjnqhqiriykkn@gmail.com>
2025-06-23 20:28:49 +0300

    test!: add word service test

---
+++ src/test/java/edu/architect_711/wordsapp/service/WordServiceIntegrationTest.java
@@ -0,0 +1,292 @@
+package edu.architect_711.wordsapp.service;
+
+import edu.architect_711.wordsapp.exception.UnauthorizedGroupModifyAttemptException;
+import edu.architect_711.wordsapp.model.dto.word.SaveWordRequest;
+import edu.architect_711.wordsapp.model.dto.word.WordDto;
+import edu.architect_711.wordsapp.model.entity.Word;
+import edu.architect_711.wordsapp.repository.AccountRepository;
+import edu.architect_711.wordsapp.repository.GroupRepository;
+import edu.architect_711.wordsapp.repository.LanguageRepository;
+import edu.architect_711.wordsapp.repository.NodeRepository;
+import edu.architect_711.wordsapp.repository.WordRepository;
+import edu.architect_711.wordsapp.service.account.AccountService;
+import edu.architect_711.wordsapp.service.group.GroupService;
+import edu.architect_711.wordsapp.service.word.DefaultWordService;
+import edu.architect_711.wordsapp.utils.Cleaner;
+import edu.architect_711.wordsapp.utils.Persister;
+
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.ConstraintViolationException;
+import jakarta.validation.Validation;
+import jakarta.validation.Validator;
+
+import java.util.Set;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static edu.architect_711.wordsapp.model.mapper.WordMapper.toDto;
+import static edu.architect_711.wordsapp.model.mapper.WordMapper.toEntity;
+import static edu.architect_711.wordsapp.model.mapper.WordMapper.toSaveRequest;
+import static edu.architect_711.wordsapp.security.utils.AuthenticationExtractor.getAccount;
+import static edu.architect_711.wordsapp.utils.TestEntityGenerator.word;
+import static edu.architect_711.wordsapp.utils.TestUtils.safeCleanAuth;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@SpringBootTest
+@TestInstance(value = TestInstance.Lifecycle.PER_CLASS)
+public class WordServiceIntegrationTest {
+    @Autowired
+    private DefaultWordService wordService;
+    @Autowired
+    private AccountService accountService;
+    @Autowired
+    private GroupService groupService;
+
+    @Autowired
+    private GroupRepository groupRepository;
+    @Autowired
+    private AccountRepository accountRepository;
+    @Autowired
+    private LanguageRepository languageRepository;
+    @Autowired
+    private WordRepository wordRepository;
+    @Autowired
+    private NodeRepository nodeRepository;
+
+    private Cleaner cleaner;
+    private Persister persister;
+
+    @BeforeAll
+    public void setup() {
+        cleaner = Cleaner.builder()
+                .groupRepository(groupRepository)
+                .accountRepository(accountRepository)
+                .nodeRepository(nodeRepository)
+                .wordRepository(wordRepository)
+                .build();
+
+        persister = Persister.builder()
+                .accountRepository(accountRepository)
+                .accountService(accountService)
+                .groupService(groupService)
+                .wordService(wordService)
+                .build();
+    }
+
+    @AfterEach
+    public void cleanup() {
+        safeCleanAuth();
+    }
+
+    /* --------------- SAVE --------------- */
+    @Test
+    public void should_ok__save() {
+        // prepare
+        var account = persister.save_auth_get_account();
+        var group = persister.safe_persist_group(getAccount());
+        var lang = languageRepository.findN(0);
+
+        // persist
+        var model = word(lang);
+        var word = assertDoesNotThrow(() -> wordService.save(toSaveRequest(model), group.getId()));
+
+        // check
+        assertNotNull(word);
+
+        compare(model, word);
+
+        assertNotNull(word.getId());
+        assertNotNull(word.getCreated());
+
+        // persist to node
+        var node = assertDoesNotThrow(() -> nodeRepository.safeFindByWordId(word.getId()));
+
+        // check whether the node was saved properly
+        assertNotNull(node);
+
+        assertEquals(node.getWord().getId(), word.getId());
+        assertEquals(node.getGroup().getId(), group.getId());
+
+        // relationship check. check group has a reference
+        var groupEntity = groupRepository.findById(group.getId());
+        var wordEntity = wordRepository.findById(word.getId());
+
+        assertTrue(groupEntity.isPresent());
+        assertTrue(wordEntity.isPresent());
+
+        var words = groupEntity.get().getWords();
+
+        assertFalse(words.isEmpty());
+
+        var equals = false;
+        for (Word w : words) {
+            if (w.getId().equals(wordEntity.get().getId())) {
+                compare(wordEntity.get(), toDto(w));
+                equals = true;
+            }
+        }
+        assertTrue(equals);
+
+        // cleanup
+        cleaner.clear(node.getId(), word.getId(), group.getId(), account.getId());
+    }
+
+    private void compare(Word a, WordDto b) {
+        assertEquals(a.getTitle(), b.getTitle());
+        assertEquals(a.getDefinition(), b.getDefinition());
+        assertEquals(a.getDescription(), b.getDescription());
+        assertEquals(a.getTranscriptions(), b.getTranscriptions());
+        assertEquals(a.getTranslations(), b.getTranslations());
+        assertEquals(a.getLanguage().getId(), b.getLanguageId());
+    }
+
+    @Test
+    public void should_fail__save__illegal_arguments() {
+        // check
+        var invalid = new SaveWordRequest(
+                null, -1L, "      \t", "", null, null);
+
+        // number of constraints
+        Validator validator = Validation.buildDefaultValidatorFactory().getValidator();
+        Set<ConstraintViolation<SaveWordRequest>> validateResult = validator.validate(invalid);
+
+        // why 2? see the SaveWordRequest!
+        assertEquals(2, validateResult.size());
+
+        // service reject
+        assertThrows(
+                ConstraintViolationException.class,
+                () -> wordService.save(invalid, 0L)); // don't care about group id
+
+        // wasn't saved
+        var found = wordRepository.findByTitle(invalid.getTitle());
+
+        assertTrue(found.isEmpty());
+    }
+
+    // when trying to save a word into another user group
+    @Test
+    public void should_fail__save__third_party_access() {
+        // prepare
+        var user1 = persister.save_auth_get_account();
+        var user1Group = persister.safe_persist_group(getAccount());
+        var lang = languageRepository.findN(0);
+
+        var word = word(lang);
+
+        // login as another user
+        var user2 = persister.save_auth_get_account();
+
+        // check
+        assertThrows(
+                UnauthorizedGroupModifyAttemptException.class,
+                () -> wordService.save(toSaveRequest(word), user1Group.getId()));
+
+        // check wasn't save
+        var foundWord = wordRepository.findByTitle(word.getTitle());
+        var foundNode = nodeRepository.findByWordId(word.getId());
+
+        assertTrue(foundWord.isEmpty());
+        assertTrue(foundNode.isEmpty());
+
+        // cleanup
+        cleaner.clear(user1Group.getId(), user1.getId());
+        accountRepository.deleteById(user2.getId());
+    }
+
+    /* --------------- GET BY ID --------------- */
+    @Test
+    public void should_ok__get_by_id() {
+        // prepare
+        var account = persister.save_auth_get_account();
+        var group = persister.safe_persist_group(getAccount());
+        var lang = languageRepository.findN(0);
+        var word = persister.safe_persist_word(lang, group.getId());
+
+        // check
+        var found = assertDoesNotThrow(() -> wordService.get(word.getId()));
+        assertTrue(found.isPresent());
+        compare(toEntity(word, lang), found.get());
+
+        // check node
+        var node = nodeRepository.findByWordId(word.getId());
+
+        assertTrue(node.isPresent());
+        assertEquals(group.getId(), node.get().getGroup().getId());
+
+        // cleanup
+        cleaner.clear(node.get().getId(), word.getId(), group.getId(), account.getId());
+    }
+
+    @Test
+    public void should_fail__get_by_id__not_found() {
+        // prepare
+        var account = persister.save_auth_get_account();
+        var group = persister.safe_persist_group(getAccount());
+
+        var faboulosWordId = 9999999L;
+
+        // check
+        var found = assertDoesNotThrow(() -> wordService.get(faboulosWordId));
+
+        assertTrue(found.isEmpty());
+
+        // cleanup
+        cleaner.clear(group.getId(), account.getId());
+    }
+
+    /* --------------- GET ALL --------------- */
+    @Test
+    public void should_ok__get_all() {
+        // prepare
+        var account = persister.save_auth_get_account();
+        var group = persister.safe_persist_group(getAccount());
+        var lang = languageRepository.findN(0);
+
+        // persist
+        var word1 = persister.safe_persist_word(lang, group.getId());
+        var word2 = persister.safe_persist_word(lang, group.getId());
+
+        // check
+        var node1 = nodeRepository.findByWordId(word1.getId());
+        var node2 = nodeRepository.findByWordId(word2.getId());
+
+        assertTrue(node1.isPresent());
+        assertTrue(node2.isPresent());
+
+        // this might fail if the database is already contained some words, don't really
+        // care about
+        var found = assertDoesNotThrow(() -> wordService.get(0, 2, group.getId()));
+
+        assertFalse(found.isEmpty());
+        assertEquals(2, found.size());
+
+        var timesMatched = 0;
+        for (WordDto f : found) {
+            if (f.getTitle().equals(word1.getTitle()) || f.getTitle().equals(word2.getTitle()))
+                timesMatched++;
+        }
+        assertEquals(2, timesMatched);
+
+        // cleanup
+        nodeRepository.deleteById(node1.get().getId());
+        nodeRepository.deleteById(node2.get().getId());
+
+        wordRepository.deleteById(word1.getId());
+        wordRepository.deleteById(word2.getId());
+
+        cleaner.clear(group.getId(), account.getId());
+    }
+}
