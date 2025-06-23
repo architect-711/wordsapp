52e1f5b650f029ca4ccf6caa812ccb6937e0254d
45p9uerhiu <jfjjjnqhqiriykkn@gmail.com>
2025-06-23 20:26:38 +0300

    refactor: clean repeating methods

--- src/main/java/edu/architect_711/wordsapp/service/group/DefaultGroupService.java
+++ src/main/java/edu/architect_711/wordsapp/service/group/DefaultGroupService.java
@@ -8,16 +8,15 @@ import edu.architect_711.wordsapp.model.entity.Group;
 import edu.architect_711.wordsapp.repository.GroupRepository;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.access.AccessDeniedException;
 import org.springframework.stereotype.Service;
 import org.springframework.validation.annotation.Validated;
 
 import java.util.List;
-import java.util.Objects;
 
 import static edu.architect_711.wordsapp.model.mapper.GroupMapper.toDto;
 import static edu.architect_711.wordsapp.model.mapper.GroupMapper.toEntity;
-import static edu.architect_711.wordsapp.security.AuthenticatedUserExtractor.getAccount;
+import static edu.architect_711.wordsapp.security.utils.AuthenticationExtractor.getAccount;
+import static edu.architect_711.wordsapp.security.utils.CheckAccess.checkGroupAccess;
 
 @Service
 @RequiredArgsConstructor
@@ -42,32 +41,20 @@ public class DefaultGroupService implements GroupService {
     public GroupDto update(@Valid UpdateGroupDto updateGroupDto) {
         Group group = groupRepository.safeFindById(updateGroupDto.getId());
 
-        checkThirdPartyAccess(group.getAccount().getId());
+        checkGroupAccess(group.getAccount().getId());
 
         group.setTitle(updateGroupDto.getTitle());
         group.setDescription(updateGroupDto.getDescription());
-        
+
         return toDto(groupRepository.save(group));
     }
 
     @Override
     public void delete(Long id) {
         var group = groupRepository.safeFindById(id);
-        checkThirdPartyAccess(group.getAccount().getId());
+        checkGroupAccess(group.getAccount().getId());
 
         groupRepository.deleteById(id);
-    }
-
-    /**
-     * Check third-party access. For example when a user1 tries to access
-     * a user2 groups
-     * @param groupAccountId the group's owner id to be affected
-     */
-    private void checkThirdPartyAccess(long groupAccountId) {
-        var account = getAccount();
-
-        if (!Objects.equals(groupAccountId, account.getId()))
-            throw new AccessDeniedException("You can't delete another user group!");
     }
 
 }
