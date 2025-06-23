874e0bde83c81279899db9ce411fc293bad1bfab
45p9uerhiu <jfjjjnqhqiriykkn@gmail.com>
2025-06-23 20:20:45 +0300

    feat: move base 64 generator

---
+++ src/main/java/edu/architect_711/wordsapp/generator/Base64Generator.java
@@ -0,0 +1,12 @@
+package edu.architect_711.wordsapp.generator;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
+
+public class Base64Generator {
+    public static String generate(String username, String password) {
+        String credentials = username + ":" + password;
+
+        return Base64.getEncoder().encodeToString(credentials.getBytes(StandardCharsets.UTF_8));
+    }
+}
