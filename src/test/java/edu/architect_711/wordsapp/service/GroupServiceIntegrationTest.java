08cee690088ee875bcb25eb7d1ffb2019e84c197
45p9uerhiu <jfjjjnqhqiriykkn@gmail.com>
2025-06-23 20:27:56 +0300

    refactor!: rewrite group test

--- src/test/java/edu/architect_711/wordsapp/service/GroupServiceIntegrationTest.java
+++ src/test/java/edu/architect_711/wordsapp/service/GroupServiceIntegrationTest.java
@@ -1,164 +1,263 @@
 package edu.architect_711.wordsapp.service;
 
+import edu.architect_711.wordsapp.exception.UnauthorizedGroupModifyAttemptException;
 import edu.architect_711.wordsapp.model.dto.group.GroupDto;
 import edu.architect_711.wordsapp.model.dto.group.SaveGroupDto;
 import edu.architect_711.wordsapp.model.dto.group.UpdateGroupDto;
 import edu.architect_711.wordsapp.repository.AccountRepository;
 import edu.architect_711.wordsapp.repository.GroupRepository;
+import edu.architect_711.wordsapp.service.account.DefaultAccountService;
 import edu.architect_711.wordsapp.service.group.GroupService;
+import edu.architect_711.wordsapp.utils.Cleaner;
+import edu.architect_711.wordsapp.utils.Persister;
 import jakarta.persistence.EntityNotFoundException;
+import jakarta.validation.ConstraintViolation;
 import jakarta.validation.ConstraintViolationException;
-import org.junit.jupiter.api.*;
+import jakarta.validation.Validation;
+import jakarta.validation.Validator;
+
+import java.util.Set;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.security.access.AccessDeniedException;
-import org.springframework.security.core.context.SecurityContextHolder;
 
-import java.time.LocalDateTime;
-import java.util.ArrayList;
-
-import static edu.architect_711.wordsapp.Authenticator.authenticate;
-import static edu.architect_711.wordsapp.model.mapper.GroupMapper.toUpdateDto;
-import static edu.architect_711.wordsapp.security.AuthenticatedUserExtractor.getAccount;
-import static org.junit.jupiter.api.Assertions.*;
+import static edu.architect_711.wordsapp.model.mapper.GroupMapper.toSaveGroupDto;
+import static edu.architect_711.wordsapp.security.utils.AuthenticationExtractor.getAccount;
+import static edu.architect_711.wordsapp.utils.Authenticator.authenticate;
+import static edu.architect_711.wordsapp.utils.TestEntityGenerator.account;
+import static edu.architect_711.wordsapp.utils.TestEntityGenerator.group;
+import static edu.architect_711.wordsapp.utils.TestEntityGenerator.updateGroupDto;
+import static edu.architect_711.wordsapp.utils.TestUtils.safeCleanAuth;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
-@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class GroupServiceIntegrationTest {
     @Autowired
     private GroupService groupService;
     @Autowired
-    private GroupRepository groupRepository;
+    private DefaultAccountService defaultAccountService;
 
+    @Autowired
+    private GroupRepository groupRepository;
     @Autowired
     private AccountRepository accountRepository;
 
-    private final SaveGroupDto UNEXISTING_GROUP_DTO = genSaveDto();
-    /**
-     * <b>NOTE!!!</b> This var MUST never be cleared or reassigned. Treat it as FINAL.
-     * It represents saved by the {@link GroupServiceIntegrationTest#should_ok__save()} method value
-     */
-    private GroupDto savedBuff = null;
-
-    public SaveGroupDto genSaveDto() {
-        return new SaveGroupDto("test_" + LocalDateTime.now(), "some description");
-    }
+    private Persister persister;
+    private Cleaner cleaner;
 
     @BeforeAll
     public void setup() {
-        authenticate(accountRepository.safeFindFirst());
+        persister = Persister.builder()
+                .accountRepository(accountRepository)
+                .accountService(defaultAccountService)
+                .groupService(groupService)
+                .build();
+        cleaner = Cleaner.builder()
+                .groupRepository(groupRepository)
+                .accountRepository(accountRepository)
+                .build();
     }
 
+    @AfterEach
+    public void cleanup() {
+        safeCleanAuth();
+    }
 
-
-    @Order(1)
+    /* --------------- SAVE --------------- */
     @Test
     public void should_ok__save() {
-        GroupDto saved = assertDoesNotThrow(() -> groupService.save(UNEXISTING_GROUP_DTO));
-        savedBuff = saved;
+        // prepare
+        var account = persister.save_auth_get_account();
 
-        assertTrue(groupRepository.findById(saved.getId()).isPresent());
+        // persist
+        var model = group(account);
+        GroupDto group = assertDoesNotThrow(() -> groupService.save(toSaveGroupDto(model)));
 
-        assertNotNull(saved);
-        assertEquals(saved.getTitle(), UNEXISTING_GROUP_DTO.getTitle());
-        assertEquals(saved.getDescription(), UNEXISTING_GROUP_DTO.getDescription());
+        // check
+        assertTrue(groupRepository.findById(group.getId()).isPresent());
+
+        assertNotNull(group);
+
+        assertEquals(model.getTitle(), group.getTitle());
+        assertEquals(model.getDescription(), group.getDescription());
+
+        // cleanup
+        cleaner.clear(group.getId(), account.getId());
     }
-    @Order(2)
+
     @Test
     public void should_fail__save__illegal_arguments() {
+        // prepare
+        var account = persister.save_auth_get_account();
+
+        // check
         var invalid = new SaveGroupDto("         \n ", null);
 
         assertThrows(ConstraintViolationException.class, () -> groupService.save(invalid));
+
+        // cleanup
+        accountRepository.deleteById(account.getId());
     }
 
-
-
-    @Order(3)
+    /* --------------- GET --------------- */
     @Test
     public void should_ok__get_all() {
-        var saved = new ArrayList<GroupDto>();
+        // prepare
+        var account = persister.save_auth_get_account();
+        var group = persister.safe_persist_group(getAccount());
 
-        final int LIMIT = 10;
-        if (groupRepository.count() < 10) {
-            for (byte i = 0; i < LIMIT; i++)
-                saved.add(groupService.save(genSaveDto()));
-        }
+        // check
+        assertNotNull(group);
 
         var found = assertDoesNotThrow(() -> groupService.get());
 
-        assertTrue(found.size() >= LIMIT);
+        assertNotNull(found);
 
+        assertTrue(found.size() >= 1);
         found.forEach(f -> assertEquals(f.getOwnerId(), getAccount().getId()));
 
-        saved.forEach(s -> groupRepository.deleteById(s.getId()));
+        // cleanup
+        cleaner.clear(group.getId(), account.getId());
     }
 
-
-
-    @Order(4)
+    /* --------------- UPDATE --------------- */
     @Test
-    public void should_ok__update() {
-        var update = new UpdateGroupDto(savedBuff.getId(), "new_test" + LocalDateTime.now(), "new description !");
+    public void should_ok__upate() {
+        // prepare
+        var account = persister.save_auth_get_account();
+        var group = persister.safe_persist_group(getAccount());
 
+        // persist
+        var update = updateGroupDto(group.getId());
         var updated = assertDoesNotThrow(() -> groupService.update(update));
 
+        // check
         assertNotNull(updated);
+
         assertEquals(updated.getTitle(), update.getTitle());
         assertEquals(updated.getDescription(), update.getDescription());
+
+        assertEquals(getAccount().getId(), updated.getOwnerId());
+
+        // cleanup
+        cleaner.clear(group.getId(), account.getId());
     }
-    @Order(5)
+
     @Test
     public void should_fail__update__fabulous_id() {
+        // prepare
+        var account = persister.save_auth_get_account();
+
+        // check
         final Long REALLY_FABULOUS_ID = 999999L;
-        var update = new UpdateGroupDto(REALLY_FABULOUS_ID, "this should not be saved in the database",
+        var invalid = new UpdateGroupDto(
+                REALLY_FABULOUS_ID,
+                "this should never be saved in the database",
                 "if it did, checkout the test files");
 
-        assertThrows(EntityNotFoundException.class, () -> groupService.update(update));
+        // since test firstly tries to find group by id, we don't have to save it before
+        assertThrows(EntityNotFoundException.class, () -> groupService.update(invalid));
+
+        var found = groupRepository.findByTitle(invalid.getTitle());
+        assertTrue(found.isEmpty());
+
+        // cleanup
+        accountRepository.deleteById(account.getId());
     }
-    @Order(6)
+
     @Test
     public void should_fail__update__illegal_arguments() {
-        var update = new UpdateGroupDto(-1L, null, "                   \0  ");
+        // prepare
+        var account = persister.save_auth_get_account();
+        var group = persister.safe_persist_group(getAccount());
 
-        assertThrows(ConstraintViolationException.class, () -> groupService.update(update));
+        Validator validator = Validation.buildDefaultValidatorFactory().getValidator();
+
+        // check
+        var invalid = new UpdateGroupDto(-1L, null, "                   \0  ");
+
+        Set<ConstraintViolation<UpdateGroupDto>> validateResult = validator.validate(invalid);
+        assertFalse(validateResult.isEmpty());
+        assertEquals(3, validateResult.size());
+
+        assertThrows(ConstraintViolationException.class, () -> groupService.update(invalid));
+
+        // check wasn't updated
+        var found = groupRepository.findById(group.getId());
+        assertTrue(found.isPresent());
+        assertEquals(group.getTitle(), found.get().getTitle());
+
+        // cleanup
+        cleaner.clear(group.getId(), account.getId());
     }
-    @Order(7)
+
     @Test
     public void should_fail__update__third_party_access() {
-        var account = accountRepository.findAll(PageRequest.of(1, 1)).getContent().getFirst(); // find second user
-        var backupAuth = SecurityContextHolder.getContext().getAuthentication(); // I hope that will not be changed
+        // prepare
+        var account = persister.save_auth_get_account();
+        var group = persister.safe_persist_group(getAccount());
 
-        authenticate(account); // login as another user
+        // login as another user
+        var accountModel2 = account();
+        var account2 = assertDoesNotThrow(() -> accountRepository.save(accountModel2));
+        authenticate(account2);
 
-        var group = groupRepository.safeFindById(savedBuff.getId()); // group of original user
+        // check
+        var firstAccountGroupToUpdate = updateGroupDto(group.getId());
 
-        assertThrows(AccessDeniedException.class, () -> groupService.update(toUpdateDto(group))); // can't access
+        assertThrows(
+                UnauthorizedGroupModifyAttemptException.class,
+                () -> groupService.update(firstAccountGroupToUpdate));
 
-        SecurityContextHolder.getContext().setAuthentication(backupAuth); // get things back
+        // cleanup
+        cleaner.clear(group.getId(), account.getId(), account2.getId());
     }
 
-
-
-    @Order(8)
-    @Test
-    public void should_fail__delete__third_party_access() {
-        var backupAuth = SecurityContextHolder.getContext().getAuthentication();
-        var anotherUser = accountRepository.findAll(PageRequest.of(1, 1)).getContent().getFirst();
-        var group = groupRepository.safeFindById(savedBuff.getId());
-
-        authenticate(anotherUser);
-
-        assertThrows(AccessDeniedException.class, () -> groupService.delete(group.getId()));
-
-        SecurityContextHolder.getContext().setAuthentication(backupAuth);
-    }
-    @Order(9)
+    /* --------------- DELETE --------------- */
     @Test
     public void should_ok__delete() {
-        assertDoesNotThrow(() -> groupService.delete(savedBuff.getId()));
+        // prepare
+        var account = persister.save_auth_get_account();
+        var group = persister.safe_persist_group(getAccount());
 
-        assertTrue(groupRepository.findById(savedBuff.getId()).isEmpty());
+        // check
+        assertDoesNotThrow(() -> groupService.delete(group.getId()));
+
+        assertTrue(groupRepository.findById(group.getId()).isEmpty());
+
+        // cleanup
+        accountRepository.deleteById(account.getId());
+    }
+
+    @Test
+    public void should_fail__delete__third_party_access() {
+        // prepare
+        var account = persister.save_auth_get_account();
+        var group = persister.safe_persist_group(getAccount());
+
+        // login as another user
+        var accountModel2 = account();
+        var account2 = assertDoesNotThrow(() -> accountRepository.save(accountModel2));
+        authenticate(account2);
+
+        // check
+        assertThrows(
+                UnauthorizedGroupModifyAttemptException.class,
+                () -> groupService.delete(group.getId()));
+
+        assertTrue(groupRepository.findById(group.getId()).isPresent());
+
+        // clenaup
+        cleaner.clear(group.getId(), account.getId(), account2.getId());
     }
 }
