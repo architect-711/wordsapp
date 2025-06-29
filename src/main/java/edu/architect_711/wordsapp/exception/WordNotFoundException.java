0dec00627ddb9b5457fba5ff4fe7482b980139c9
45p9uerhiu <jfjjjnqhqiriykkn@gmail.com>
2025-06-29 15:17:27 +0300

    chore: delete unused files

--- src/main/java/edu/architect_711/wordsapp/exception/WordNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package edu.architect_711.wordsapp.exception;
-
-import jakarta.persistence.EntityNotFoundException;
-
-public class WordNotFoundException extends EntityNotFoundException {
-    public WordNotFoundException(String message) {
-        super(message);
-    }
-}
