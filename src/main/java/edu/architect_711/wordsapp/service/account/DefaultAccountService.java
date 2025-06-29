5c4b42c7c506ac20e7d7db771e49e6ae116783d1
45p9uerhiu <jfjjjnqhqiriykkn@gmail.com>
2025-06-29 16:46:13 +0300

    refactor: update service methods

--- src/main/java/edu/architect_711/wordsapp/service/account/DefaultAccountService.java
+++ src/main/java/edu/architect_711/wordsapp/service/account/DefaultAccountService.java
@@ -1,9 +1,8 @@
 package edu.architect_711.wordsapp.service.account;
 
-import edu.architect_711.wordsapp.model.dto.account.AccountDto;
-import edu.architect_711.wordsapp.model.dto.account.AccountLoginRequest;
-import edu.architect_711.wordsapp.model.dto.account.SaveAccountDto;
+import edu.architect_711.wordsapp.model.dto.account.*;
 import edu.architect_711.wordsapp.model.entity.Account;
+import edu.architect_711.wordsapp.model.mapper.AccountMapper;
 import edu.architect_711.wordsapp.repository.AccountRepository;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -13,10 +12,13 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.validation.annotation.Validated;
 
+import java.util.Optional;
+
 import static edu.architect_711.wordsapp.generator.Base64Generator.generate;
 import static edu.architect_711.wordsapp.model.mapper.AccountMapper.toDto;
 import static edu.architect_711.wordsapp.model.mapper.AccountMapper.toEntity;
 import static edu.architect_711.wordsapp.security.utils.AuthenticationExtractor.getAccount;
+import static edu.architect_711.wordsapp.security.utils.AuthenticationExtractor.safeGetAccount;
 
 @Service
 @RequiredArgsConstructor
@@ -26,7 +28,7 @@ public class DefaultAccountService implements AccountService {
     private final PasswordEncoder passwordEncoder;
 
     @Override
-    public AccountDto save(@Valid SaveAccountDto accountDto) { // TODO mb return base64 token?
+    public AccountDto save(@Valid SaveAccountDto accountDto) {
         Account account = toEntity(accountDto);
         account.setPassword(passwordEncoder.encode(accountDto.getPassword()));
 
@@ -34,35 +36,45 @@ public class DefaultAccountService implements AccountService {
     }
 
     @Override
-    public String login64(AccountLoginRequest accountLoginRequest) {
+    public AuthBaseTokenResponse save64(@Valid SaveAccountDto saveAccountDto) {
+        save(saveAccountDto);
+
+        return buildAuthResponse(saveAccountDto.getUsername(), saveAccountDto.getPassword());
+    }
+
+    @Override
+    public AuthBaseTokenResponse login64(@Valid AccountLoginRequest accountLoginRequest) {
         Account account = accountRepository.safeFindByUsername(accountLoginRequest.getUsername());
 
         if (!passwordEncoder.matches(accountLoginRequest.getPassword(), account.getPassword()))
             throw new BadCredentialsException("Wrong password");
 
-        return generate(account.getUsername(), account.getPassword());
+        return buildAuthResponse(account.getUsername(), accountLoginRequest.getPassword());
+    }
+
+    private AuthBaseTokenResponse buildAuthResponse(String username, String password) {
+        return new AuthBaseTokenResponse(generate(username, password));
     }
 
     @Override
-    public AccountDto get() {
-        return toDto(getAccount());
+    public Optional<AccountDto> get() {
+        return Optional.ofNullable(getAccount()).map(AccountMapper::toDto);
     }
 
     @Override
     public void delete() {
-        accountRepository.deleteById(getAccount().getId());
+        accountRepository.deleteById(safeGetAccount().getId());
         SecurityContextHolder.clearContext();
     }
 
     @Override
-    public AccountDto update(@Valid AccountDto accountDto) {
-        Account account = getAccount();
+    public AccountDto update(@Valid UpdateAccountRequest accountDto) {
+        Account account = safeGetAccount();
 
         account.setEmail(accountDto.getEmail());
         account.setUsername(accountDto.getUsername());
 
         return toDto(accountRepository.save(account));
     }
-
 
 }
